71aedd632376909c9f7b266817d7d654bd899fc4
Lukas Adamek <lukas.adamek@cern.ch>
2018-10-18 14:06:48 +0200

    Added event cleaning and core flags cut

--- scripts/config_eop_tree_dump_data.py
+++ scripts/config_eop_tree_dump_data.py
@@ -36,7 +36,7 @@ c.setalg("BasicEventSelection", {"m_name": "BasicEventSelection",
 c.setalg("TrackHistsAlgo", {"m_name": "Tracks_BasicEvtSel",
                             "m_inContainerName": trks,
                             "m_detailStr": "2D IPDetails HitCounts Chi2Details",
-                            "m_msgLevel": "debug"})
+                            "m_msgLevel": "info"})
 
 
 '''track selection algorithm'''
@@ -45,44 +45,44 @@ c.setalg("InDetTrackSelectionToolAlgo", {"m_name": "Sel_" + trks_loose,
                                   "m_minPt": 0.5,
                                   "m_CutLevel": "Loose",
                                   "m_outputTrackContainer": trks_loose,
-                                  "m_msgLevel": "debug"})
+                                  "m_msgLevel": "info"})
 
 ''' Fill histograms with tracking details, after LoosePrimary selection '''
 c.setalg("TrackHistsAlgo", {"m_name": "TrackHist_" + trks_loose,
                             "m_inContainerName": trks_loose,
                             "m_detailStr": "2D IPDetails HitCounts Chi2Details",
-                            "m_msgLevel": "debug"})
+                            "m_msgLevel": "info"})
 
 c.setalg("TrackExtrapolationIsolationTool", {"m_name": "TrackIso_" + trks_loose,
                                         "m_inputTrackContainer": trks_loose,
                                         "m_outputTrackContainer": trks_loose_isolated,
                                         "m_trkIsoDRmax": 0.4,
-                                        "m_msgLevel": "debug"})
+                                        "m_msgLevel": "info"})
 
 ''' Fill histograms with tracking details, after LoosePrimary selection '''
 c.setalg("TrackHistsAlgo", {"m_name": "TrackHist_" + trks_loose_isolated,
                             "m_inContainerName": trks_loose_isolated,
                             "m_detailStr": "2D IPDetails HitCounts Chi2Details",
-                            "m_msgLevel": "debug"})
+                            "m_msgLevel": "info"})
 
 c.setalg("InDetTrackSelectionToolAlgo", {"m_name": "Sel_" + trks_tight_isolated ,
                                     "m_inputTrackContainer": trks_loose_isolated,
                                     "m_outputTrackContainer": trks_tight_isolated,
                                     "m_CutLevel": "TightPrimary",
-                                    "m_msgLevel": "debug"})
+                                    "m_msgLevel": "info"})
 
 ''' Fill histograms with tracking details, after LoosePrimary selection '''
 c.setalg("TrackHistsAlgo", {"m_name": "TrackHist_" + trks_tight_isolated,
                             "m_inContainerName": trks_tight_isolated,
                             "m_detailStr": "2D IPDetails HitCounts Chi2Details",
-                            "m_msgLevel": "debug"})
+                            "m_msgLevel": "info"})
 
 c.setalg("TightTrackVertexAssociationToolAlgo", {"m_name":"TrackVertexAssociationTool",\
                                             "m_inputTrackContainer": trks_loose_isolated,\
                                             "m_outputTrackContainer": trks_loose_isolated_vertex,\
                                             "m_dzSinTheta_cut": 1.5,
                                             "m_d0_cut": 1.5,
-                                            "m_msgLevel": "debug",
+                                            "m_msgLevel": "info",
                                             })
 
 c.setalg("TightTrackVertexAssociationToolAlgo", {"m_name":"TrackVertexAssociatedTool",\
@@ -90,7 +90,7 @@ c.setalg("TightTrackVertexAssociationToolAlgo", {"m_name":"TrackVertexAssociated
                                             "m_outputTrackContainer": trks_tight_isolated_vertex,\
                                             "m_dzSinTheta_cut":1.5,
                                             "m_d0_cut":1.5,
-                                            "m_msgLevel": "debug",
+                                            "m_msgLevel": "info",
                                             })
 
 #### Make E/p ttree
@@ -101,5 +101,5 @@ for track_container in [trks_loose_isolated, trks_loose_isolated_vertex, trks_ti
                                     "m_inTrackContainerName": track_container,
                                     "m_energyCalib": energy_calib, # ClusterEnergy, ClusterEnergyLCW, or CellEnergy
                                     "m_useCutFlow": True,
-                                    "m_msgLevel": "debug"})
+                                    "m_msgLevel": "info"})
 
