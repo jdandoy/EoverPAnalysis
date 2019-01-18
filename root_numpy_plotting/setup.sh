a5dadf0e2fe63671e3ced787c2f979a29dd6f35c
Lukas Adamek <lukas.adamek@cern.ch>
2019-01-18 20:47:42 +0100

    Fixed small mistakes in setup script

--- root_numpy_plotting/setup.sh
+++ root_numpy_plotting/setup.sh
@@ -1,19 +1,19 @@
 
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh
-export EOPPlottingDir=$(pwd)
 asetup AnalysisBase,21.2.23
 
 #pip uninstall cython
 #pip uninstall root_numpy
 #pip uninstall psutil
 
+export EOPPlottingDir=$PWD
 mkdir -p $PWD/CondorPythonLocal/lib/python2.7/site-packages
 export PYTHONPATH=$PWD/CondorPythonLocal/lib/python2.7/site-packages:$PYTHONPATH
 export BLAS=None LAPACK=None ATLAS=None
 
-pip install --install-option="--prefix=$PWD/CondorPythonLocal" --ignore-installed cython
-pip install --install-option="--prefix=$PWD/CondorPythonLocal" --ignore-installed root_numpy
-pip install --install-option="--prefix=$PWD/CondorPythonLocal" --ignore-installed psutil 
+#pip install --install-option="--prefix=$PWD/CondorPythonLocal" --ignore-installed cython
+#pip install --install-option="--prefix=$PWD/CondorPythonLocal" --ignore-installed root_numpy
+#pip install --install-option="--prefix=$PWD/CondorPythonLocal" --ignore-installed psutil 
 
 echo "READY TO GO!"
