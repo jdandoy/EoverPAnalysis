2a1e34b5f5f2703922240029b44466a2a9d42d80
luadamek <lukas.adamek@cern.ch>
2019-05-13 16:21:32 -0400

    Added histograms to reweight pT spectrum in Pythia and single pion to low mu data

---
+++ Plotting/PtBinReweighting.sh
@@ -0,0 +1,11 @@
+python ReweightingHistograms/CreateReweightingHistogram.py --file Plots.root --numerator LowMuData --denomenator PythiaJetJet --histogramName TrackPtSpectrum__Inclusive_Eta_0 --outputName PtSpectrumReweightLowMuDataOverPythiaJetJet_Eta0
+python ReweightingHistograms/CreateReweightingHistogram.py --file Plots.root --numerator LowMuData --denomenator PythiaJetJet --histogramName TrackPtSpectrum__Inclusive_Eta_0 --outputName PtSpectrumReweightLowMuDataOverPythiaJetJet_Eta1
+python ReweightingHistograms/CreateReweightingHistogram.py --file Plots.root --numerator LowMuData --denomenator PythiaJetJet --histogramName TrackPtSpectrum__Inclusive_Eta_0 --outputName PtSpectrumReweightLowMuDataOverPythiaJetJet_Eta2
+python ReweightingHistograms/CreateReweightingHistogram.py --file Plots.root --numerator LowMuData --denomenator PythiaJetJet --histogramName TrackPtSpectrum__Inclusive_Eta_0 --outputName PtSpectrumReweightLowMuDataOverPythiaJetJet_Eta3
+python ReweightingHistograms/CreateReweightingHistogram.py --file Plots.root --numerator LowMuData --denomenator PythiaJetJet --histogramName TrackPtSpectrum__Inclusive_Eta_0 --outputName PtSpectrumReweightLowMuDataOverPythiaJetJet_Eta4
+
+python ReweightingHistograms/CreateReweightingHistogram.py --file Plots.root --numerator LowMuData --denomenator SinglePion --histogramName TrackPtSpectrum__Inclusive_Eta_0 --outputName PtSpectrumReweightLowMuDataOverSinglePion_Eta0
+python ReweightingHistograms/CreateReweightingHistogram.py --file Plots.root --numerator LowMuData --denomenator SinglePion --histogramName TrackPtSpectrum__Inclusive_Eta_0 --outputName PtSpectrumReweightLowMuDataOverSinglePion_Eta1
+python ReweightingHistograms/CreateReweightingHistogram.py --file Plots.root --numerator LowMuData --denomenator SinglePion --histogramName TrackPtSpectrum__Inclusive_Eta_0 --outputName PtSpectrumReweightLowMuDataOverSinglePion_Eta2
+python ReweightingHistograms/CreateReweightingHistogram.py --file Plots.root --numerator LowMuData --denomenator SinglePion --histogramName TrackPtSpectrum__Inclusive_Eta_0 --outputName PtSpectrumReweightLowMuDataOverSinglePion_Eta3
+python ReweightingHistograms/CreateReweightingHistogram.py --file Plots.root --numerator LowMuData --denomenator SinglePion --histogramName TrackPtSpectrum__Inclusive_Eta_0 --outputName PtSpectrumReweightLowMuDataOverSinglePion_Eta4
