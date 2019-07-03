d6f0e23f3dffb21129f5c6a60f2502be378449fd
luadamek <lukas.adamek@cern.ch>
2019-07-03 14:03:54 -0400

    Update copied figures

--- Plotting/CopyPlotsToIntNote.py
+++ Plotting/CopyPlotsToIntNote.py
@@ -8,9 +8,9 @@ plot_dir = os.path.join(os.getenv("EOPPlottingDir"), "Plots", "PTSpectrumReweigh
 plot_list = ["EtaLess08_TwoDHistTrkPvsPhiInnerToExtrapolEM2LowMuData.png"]
 
 for i in range(0, 5):
-    plot_list += ["EOPDistribution_Inclusive_Eta_{}_Inclusive.png".format(i)]
-    plot_list += ["EOPProfileVsMomentum_20TRTHitsNonZeroEnergy_Eta_{}_20TRTHitsNonZeroEnergy.png".format(i)]
-    plot_list += ["EOPProfileVsMomentum_MIPSelectionHadFracAbove70_Eta_{}_MIPSelectionHadFracAbove70.png".format(i)]
+    plot_list += ["EOPDistribution_Inclusive_Eta_{}.png".format(i)]
+    plot_list += ["EnergyBkgProfileVsMomentum__MIPSelectionHadFracAbove70_Eta_{}_MIPSelectionHadFracAbove70.png".format(i)]
+    plot_list += ["EnergyProfileVsMomentum__MIPSelectionHadFracAbove70_Eta_{}_MIPSelectionHadFracAbove70.png".format(i)]
     plot_list += ["NonZeroFractionNonZeroEnergyInclusiveTrackPSpectrum_{}.png".format(i)]
     plot_list += ["NonZeroFraction20TRTHitsNonZeroEnergy20TRTHitsTrackPSpectrum_{}.png".format(i)]
 
