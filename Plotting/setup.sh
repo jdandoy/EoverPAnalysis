ba3cbc25e7dacf6b3351e102d5bb4af83ce7bcfa
Lukas Adamek <lukas.adamek@cern.ch>
2019-07-22 17:55:04 +0200

    Add latex installation

--- Plotting/setup.sh
+++ Plotting/setup.sh
@@ -5,5 +5,6 @@ export EOPPlottingDir=$PWD
 source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_94python3 x86_64-centos7-gcc62-opt
 export PYTHONPATH="${EOPPlottingDir}/eop_plotting:$PYTHONPATH"
 export PYTHONPATH="${EOPPlottingDir}/utils:$PYTHONPATH"
+export PATH=/cvmfs/sft.cern.ch/lcg/external/texlive/2016/bin/x86_64-linux:$PATH
 
 echo "READY TO GO!"
