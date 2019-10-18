c1b3cbae2d8132e7993f7636b05d0225a4dde1c9
Lukas Adamek <lukas.adamek@cern.ch>
2019-10-18 23:26:18 +0200

    Option to rebin reweighting histograms

--- Plotting/ReweightingHistograms/CreateReweightingHistogram.py
+++ Plotting/ReweightingHistograms/CreateReweightingHistogram.py
@@ -11,6 +11,8 @@ parser = argparse.ArgumentParser(description='Create a root file that has a hist
 parser.add_argument('--file', '-f', dest="file", type=str, required=True, help='Create a histogram required for reweighting')
 parser.add_argument('--numerator', '-num', dest="num", type=str, required=True, help='The channel to reweight to')
 parser.add_argument('--denomenator', '-den', dest="den", type=str, required=True, help='The channel to be reweighted')
+parser.add_argument('--rebin_x', '-rbx', dest="rbx", type=int, required=False, default=0, help='The rebinning to be applied to the x-axis')
+parser.add_argument('--rebin_y', '-rby', dest="rby", type=int, required=False, default=0, help='The rebinning to be applied to the y-axis')
 parser.add_argument('--histogramName', '-hm', dest="hist_name", type=str, required=True, help='The name of the histogram to use for reweighting')
 parser.add_argument('--outputName', '-on', dest="output_name", type=str, required=True, help='the name of the root file and histogram.')
 
@@ -23,8 +25,16 @@ output_name = args.output_name
 hist_name = args.hist_name
 
 hm = HistogramManager(filename)
+hm.listHistograms()
 
 histograms = hm.getHistograms(hist_name)
+
+if args.rbx != 0:
+    for hist in histograms:
+        histograms[hist].RebinX(args.rbx)
+if args.rby != 0:
+    for hist in histograms:
+        histograms[hist].RebinY(args.rby)
 
 #numerator:
 ratio_hist = histograms[num_channel].Clone(output_name)
