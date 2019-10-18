82533538eca03cebcd7c2b481b56cb45774c40d6
Lukas Adamek <lukas.adamek@cern.ch>
2019-10-18 23:27:18 +0200

    Add support for 2d reweighting

--- Plotting/utils/utils_cython.pyx
+++ Plotting/utils/utils_cython.pyx
@@ -25,3 +25,17 @@ cpdef  np.ndarray[FLOATDTYPE_t, ndim =1] get_weights_from_bins(variable_in_histo
             if (variable_in_histogram[j] >= low_edges[i]) and (variable_in_histogram[j] < high_edges[i]):
                 weights[j] = normalization[i]
     return weights
+
+cpdef  np.ndarray[FLOATDTYPE_t, ndim =1] get_weights_from_2dbins(variable_in_histogram_type xvariable_in_histogram, variable_in_histogram_type yvariable_in_histogram, np.ndarray[FLOATDTYPE_t, ndim = 1] xlow_edges, np.ndarray[FLOATDTYPE_t, ndim=1] xhigh_edges, np.ndarray[FLOATDTYPE_t, ndim = 1] ylow_edges, np.ndarray[FLOATDTYPE_t, ndim=1] yhigh_edges, np.ndarray[FLOATDTYPE_t, ndim = 2] normalization):
+    cdef int xmaxbin = xlow_edges.shape[0]
+    cdef int ymaxbin = ylow_edges.shape[0]
+    cdef int tracks = xvariable_in_histogram.shape[0]
+    cdef int ix,iy,j
+    cdef np.ndarray[FLOATDTYPE_t, ndim =1] weights = np.ones(tracks, dtype = np.float)
+
+    for ix in range(xmaxbin):
+        for iy in range(ymaxbin):
+           for j in range(tracks):
+               if (xvariable_in_histogram[j] >= xlow_edges[ix]) and (xvariable_in_histogram[j] < xhigh_edges[ix]) and (yvariable_in_histogram[j] >= ylow_edges[iy]) and (yvariable_in_histogram[j] < yhigh_edges[iy]):
+                   weights[j] = normalization[ix,iy]
+    return weights
