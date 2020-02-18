818e1fb6a0f056dcb6bf44a914aa1cd3239638aa
Lukas Adamek <lukas.adamek@cern.ch>
2020-02-18 22:59:51 +0100

    Test fits

--- Plotting/fit_ep_histograms.py
+++ Plotting/fit_ep_histograms.py
@@ -30,21 +30,33 @@ def get_histograms(tf, binnings, hist_manager, base_name = "EOPDistribution", se
             histograms[histogram_name]["histograms"] = hist_manager.getHistograms(histogram_name)
             histograms[histogram_name]["eta_bins"] = (eta_low, eta_high)
             histograms[histogram_name]["p_bins"] = (p_bins_low, p_bins_high)
+            histograms[histogram_name]["p_count"] = p_count
+            histograms[histogram_name]["eta_count"] = eta_count
     return histograms
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--root_file", dest="root_file", required=False, default="inclusive.root")
-parser.add_argument("--selection", dest="selection", required=False, default="Inclusive")
+parser.add_argument("--selection", dest="selection", required=False, default="NonZeroEnergy")
 args = parser.parse_args()
 root_file = args.root_file
 selection = args.selection
 
-balance_fitter = ROOT.JES_BalanceFitter()
+nsigma = 1.1
+fitter = ROOT.JES_BalanceFitter(nsigma)
 hm = histogram_manager.HistogramManager(args.root_file)
 tf = ROOT.TFile(args.root_file, "READ")
-tf.ls()
 binnings = get_binning(tf, selection)
-print("These were the binnings {}".format(binnings))
 histograms = get_histograms(tf, binnings, hm, "EOPDistribution", selection = args.selection)
-print(histograms)
+
+for h in histograms:
+    canvas = ROOT.TCanvas(h, h)
+    needed_for_fit = histograms[h]
+    these_histograms = needed_for_fit["histograms"]
+    eta_bins = needed_for_fit["eta_bins"]
+    p_bins = needed_for_fit["p_bins"]
+    p_count = needed_for_fit["p_count"]
+    eta_count = needed_for_fit["eta_count"]
+    for process in these_histograms:
+        to_fit = these_histograms[process]
+        fitter.FitAndDraw(to_fit,0.0);
+        canvas.Print("{}.eps".format(to_fit.GetName()))
